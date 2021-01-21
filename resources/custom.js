9383cfb4175b8fc44a97013791a861e423b8d0de
soulartz0228 <63761018+soulartz0228@users.noreply.github.com>
2021-01-21 19:26:41 +0900

    Update custom.js

--- resources/custom.js
+++ resources/custom.js
@@ -2,3 +2,16 @@ if (window.location.protocol == "http:") {
         var restOfUrl = window.location.href.substr(5);
         window.location = "https:" + restOfUrl;
     }
+
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
